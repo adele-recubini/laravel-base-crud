b68ec4242626f6603b7e11e84c045354eeae6581
adele <adele.r87@gmail.com>
2021-03-18 18:42:45 +0100

    bonus

--- resources/views/beers/index.blade.php
+++ resources/views/beers/index.blade.php
@@ -1,1 +1,45 @@
-{{$beers}}
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{asset('css/app.css') }}" type="text/css">
+
+        <title>Birre</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+      <h1>BIRRE</h1>
+
+      <table class="table">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Type</th>
+      <th scope="col">Price</th>
+      <th scope="col">Cover</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($beers as $beer)
+
+
+    <tr>
+       <th scope="row">1</th>
+       <td></td>
+       <td></td>
+       <td></td>
+     </tr>
+    @endforeach
+
+  </tbody>
+</table>
+
+
+
+
+
+    </body>
+</html>
