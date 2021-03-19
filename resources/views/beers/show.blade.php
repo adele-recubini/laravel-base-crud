a1fa8279de83c304bd0966e8eb7ce18a8073f805
adele <adele.r87@gmail.com>
2021-03-19 16:46:38 +0100

    modifiche

--- resources/views/beers/show.blade.php
+++ resources/views/beers/show.blade.php
@@ -1,1 +1,30 @@
-{{$beer}}
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{asset('css/app.css') }}" type="text/css">
+
+        <title>Birre</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+      <h1>DETTAGLIO BIRRA</h1>
+
+
+
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="{{$beer->image}}" alt="Card image cap">
+  <div class="card-body">
+    <h4 class="card-title">{{$beer->name}}</h4>
+    <h3 class="card-title">{{$beer->type}}</h3>
+    <h2 class="card-title">{{$beer->price}}</h2>
+
+    <a href="/beers" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+    </body>
+</html>
